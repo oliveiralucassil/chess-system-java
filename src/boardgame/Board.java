fe8d8c07f43f34ae3fa34a0b404dff8bc0137c65
Lucas Oliveira <lucas7oliveira.sil@gmail.com>
2022-05-02 13:11:32 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,10 @@ public class Board {
 		this.colums = colums;
 	}
 
+	public Piece piece(int row, int colum) {
+		return pieces[row][colum];
+	}
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColum()];
+	}
 }
